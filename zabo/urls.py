15cb569e091cca7e4288dd4a71dc0c0172ad3bb6
Kim Yoonseo <kimyoonseo@kaist.ac.kr>
2018-04-25 17:50:59 +0900

    core api 적용 진행중

--- zabo/urls.py
+++ zabo/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title='Pastebin API')
 
 urlpatterns = [
     url(r'^api/', include('api.urls')),
+    url(r'^schema/$', schema_view),
 ]
 
 if settings.DEBUG:
