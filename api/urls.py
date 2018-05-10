3c23b45ddf316160ad66031441518bb568c356b5
jara <jara0701@gmail.com>
2018-05-10 10:59:55 +0900

    apply django-swagger library to project

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import include, url
 from django.urls import path, re_path
+from rest_framework_swagger.views import get_swagger_view
+
+swagger_view = get_swagger_view(title="Pastebian API")
+
 
 urlpatterns = (
     path('zaboes/', include('api.zaboes.urls')),
     path('users/', include('api.users.urls')),
+    url(r'^swagger/$', swagger_view),
+
 )
