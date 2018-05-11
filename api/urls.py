da0230884c651c58d2d6ec7a6df56288e63ba4f9
jara <jara0701@gmail.com>
2018-05-11 23:24:25 +0900

    urls.py code changed

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,14 @@
 from django.conf.urls import include, url
 from django.urls import path, re_path
 from rest_framework_swagger.views import get_swagger_view
+from api.zaboes.routers import  *
 
 swagger_view = get_swagger_view(title="Pastebian API")
 
-
-from api.zaboes.routers import  *
 urlpatterns = (
 
     url(r'^api/', include(router.urls)),
     path('users/', include('api.users.urls')),
-    url(r'^swagger/$', swagger_view),
+    # url(r'^swagger/$', swagger_view),
 
 )
