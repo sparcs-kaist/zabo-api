b26f3a6a82c9e8fdd99f326acc37f7f3ebb9e625
jara <jara0701@gmail.com>
2018-05-16 17:18:46 +0900

    apply paginatior class and implement filtering by category

--- api/zaboes/views.py
+++ api/zaboes/views.py
@@ -20,11 +20,19 @@ class ZaboViewSet(viewsets.ModelViewSet):
     """
     serializer_class = ZaboSerializer
     queryset = Zabo.objects.all()
+    filter_fields = ('category',)
+
     # permission_classes = (IsAuthenticated, )
 
-
     def list(self, request):
-        serializer = ZaboSerializer(self.queryset, many=True, context={'request': request})
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
     def perform_create(self, serializer):
