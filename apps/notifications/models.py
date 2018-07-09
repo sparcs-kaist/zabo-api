bdfb4b4ba154f1551bd59605aca415f5952d8ce6
sparcsJara <jara0701@gmail.com>
2018-07-09 16:23:07 +0900

    model 생성

--- apps/notifications/models.py
+++ apps/notifications/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from apps.common.models import TimeStampedModel
+from apps.users.models import ZaboUser
 
 # Create your models here.
+
+class BaseNotification(TimeStampedModel):
+    to = models.ForeignKey(
+        ZaboUser, on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        abstract = True
+
+class FollowingNotification(BaseNotification):
+    following = models.ForeignKey(
+        ZaboUser, on_delete=models.CASCADE,
+    )
+
