b80cb65d78b11f5abf6582812c2dbe4828134683
sparcsJara <jara0701@gmail.com>
2018-07-13 17:07:30 +0900

    move function to helpers

--- apps/notifications/helpers.py
+++ apps/notifications/helpers.py
@@ -1,6 +1,8 @@
 from apps.notifications.models import ZaboReactionNotification, CommentReactionNotification, ZaboFollowingNotification, \
     SomeoneFollowingNotification
 from apps.zaboes.models import Zabo, Comment, Recomment
+from itertools import chain
+from api.zaboes.serializers import ZaboUrlSerializer
 
 
 class ReactionNotificatinoHelper():
@@ -82,3 +84,54 @@ class SomeoneFollowingNotificatinoHelper():
     def notify_to_User(self):
         instance = SomeoneFollowingNotification(to=self.notifier, following=self.following)
         instance.save()
+
+
+# get querysets from various noti models & combine them  & sort them by updated time
+def get_sorted_noti_list_by_user(user):
+    ZaboReactors = ZaboReactionNotification.objects.filter(to=user)
+    CommentReactors = CommentReactionNotification.objects.filter(to=user)
+    ZaboFollowings = ZaboFollowingNotification.objects.filter(to=user)
+    SomeoneFollowings = SomeoneFollowingNotification.objects.filter(to=user)
+    result_list = sorted(
+        chain(ZaboReactors, CommentReactors, ZaboFollowings, SomeoneFollowings),
+        key=lambda instance: instance.updated_time, reverse=True)
+    return result_list
+
+
+def convert_noti_list_to_queryset(request, noti_list):
+    ret = []
+    context = {"request": request}
+    try:
+        for noti in noti_list:
+            ret_noti = {}
+            if isinstance(noti, ZaboReactionNotification):
+                serializer = ZaboUrlSerializer(noti.zabo, context=context)
+                ret_noti["reactors_count"] = noti.reactors_count
+                ret_noti["content"] = noti.content
+                ret_noti["from"] = noti.reactors.first().nickName
+                ret_noti["url"] = serializer.data["url"]
+                ret_noti["type"] = "ZaboReaction"
+            elif isinstance(noti, CommentReactionNotification):
+                serializer = ZaboUrlSerializer(noti.comment.zabo, context=context)
+                ret_noti["reactors_count"] = noti.reactors_count
+                ret_noti["content"] = noti.content
+                ret_noti["from"] = noti.reactors.first().nickName
+                ret_noti["url"] = serializer.data["url"]
+                ret_noti["type"] = "CommentReaction"
+            elif isinstance(noti, ZaboFollowingNotification):
+                serializer = ZaboUrlSerializer(noti.zabo, context=context)
+                ret_noti["from"] = noti.following.nickName
+                ret_noti["content"] = noti.content
+                ret_noti["url"] = serializer.data["url"]
+                ret_noti["type"] = "ZaboFollowing"
+            elif isinstance(noti, SomeoneFollowingNotification):
+                ret_noti["from"] = noti.following.nickName
+                ret_noti["type"] = "SomeoneFollowing"
+            else:
+                print(noti, 'unexpected type notification')
+                raise StopIteration
+            ret_noti["updated_at"] = noti.updated_time
+            ret.append(ret_noti)
+        return ret
+    except StopIteration:
+        print("well stopped")
