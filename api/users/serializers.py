6f5edc2816509ea628b7b121315919bb1b0896b6
sparcsJara <jara0701@gmail.com>
2018-07-14 21:18:40 +0900

    change field of userserializer

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -7,7 +7,7 @@ from apps.users.models import ZaboUser
 class ZabouserSerializer(serializers.ModelSerializer):
     class Meta:
         model = ZaboUser
-        fields = '__all__'
+        exclude = ('is_staff', 'phone', 'password')
         read_only_fields = (
             'joined_date',
         )  # auto_now_add나 auto_now가 true이면 read_only_fields여야 함.
