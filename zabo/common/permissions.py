8475b63c1c64ea02d6f78bfcd43726a4bfb0ef5e
sparcsJara <jara0701@gmail.com>
2018-07-15 01:45:40 +0900

    make  IsOwnerOrIsAuthenticatdThenCreateOnlyOrReadOnly

--- zabo/common/permissions.py
+++ zabo/common/permissions.py
@@ -1,5 +1,5 @@
 from rest_framework import permissions
-
+from operator import eq
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """
@@ -14,6 +14,27 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 
         # Write permissions are only allowed to the owner
         return obj.owner == request.user
+
+
+class IsOwnerOrIsAuthenticatdThenCreateOnlyOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        UPDATE_METHODS = ('PUT', 'PATCH')
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        elif eq(request.method, 'POST'):
+            return  request.user and request.user.is_authenticated
+        elif request.method in UPDATE_METHODS:
+            return obj.owner == request.user
+
+        # Other method does not permissioned.
+        return False
 
 
 class IsAuthenticated(permissions.IsAuthenticated):
