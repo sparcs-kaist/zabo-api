50cbfc07606e2600095a6f7197fefa0e987fe5fc
Kim Yoonseo <kimyoonseo@kaist.ac.kr>
2018-07-24 01:22:40 +0900

    한 유저에 대해 팔로잉하는 사람들 profile_image 추가

--- api/users/serializers.py
+++ api/users/serializers.py
@@ -3,8 +3,17 @@ from rest_framework import serializers
 
 from apps.users.models import ZaboUser
 
+class ZabouserFollowingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ZaboUser
+        fields = (
+            'id',
+            'profile_image',
+        )
 
 class ZabouserSerializer(serializers.ModelSerializer):
+    following = ZabouserFollowingSerializer(many=True)
+
     class Meta:
         model = ZaboUser
         exclude = ('is_staff', 'phone', 'password')
