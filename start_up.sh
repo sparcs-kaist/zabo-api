9807f875408f01bdf354442183ea73cf71c85288
joyb <kimyoonseo@kaist.ac.kr>
2018-08-09 22:00:12 +0900

    folder name zabo-api changed to zabo_api

--- start_up.sh
+++ start_up.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-cd /home/zabo/zabo-api
+cd /home/zabo/zabo_api
 python3 manage.py makemigrations
 python3 manage.py migrate
 uwsgi --ini uwsgi.ini &
-cd /home/zabo/zabo-api/nginx_config
+cd /home/zabo/zabo_api/nginx_config
 cp default2 /etc/nginx/sites-available/default2
 cd /etc/nginx/sites-enabled
 ln -s /etc/nginx/sites-available/default2 default2  
