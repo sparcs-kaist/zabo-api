c41b6cc098ffed2a17127c3f388981040e6e8310
root <sparcsJara>
2018-09-04 16:55:04 +0900

    poster file name can be written by korean

--- docker_run.sh
+++ docker_run.sh
@@ -1,2 +1,1 @@
-docker run -it -v /home/jara/zabo/zabo_api/zabo_api:/home/zabo/zabo_api -p 16135:8000 vk8520/zabo-base:0.4
-
+docker run -it -v /home/jara/zabo/zabo_api/zabo_api:/home/zabo/zabo_api -p 16135:8000 -e LANG="ko_KR.UTF_8" vk8520/zabo-base:0.6
