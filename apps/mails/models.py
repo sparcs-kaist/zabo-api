d1288cdc3e19b102734a6bd25b9d005cf6b2c985
sparcsJara <jara0701@gmail.com>
2018-09-10 16:58:10 +0900

    design models

--- apps/mails/models.py
+++ apps/mails/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+from apps.common.models import TimeStampedModel
+
+class ZaboMail(TimeStampedModel):
+    sender_address =  models.CharField(blank=False, null=False, max_length=50 , default="")
+    receivers_address= models.TextField(blank=True, null=True)
+    subject = models.CharField(max_length=140, default="Title")
+    message = models.TextField(blank=True, null=True)
+
+    def set_receivers_address(self, mail_list):
+        self.receivers_address = ':'.join(mail_list)
+
+    def get_receivers_addresss(self):
+        return self.receivers_address.split(':')
+
 # Create your models here.
