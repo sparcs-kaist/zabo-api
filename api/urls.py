26152dacc6aaaeb35bf949c1e3d48210b62f77ef
sparcsJara <jara0701@gmail.com>
2018-09-10 18:00:31 +0900

    mail routre url 추가

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from rest_framework_swagger.views import get_swagger_view
 from api.users.routers import zabouser_router
 from api.zaboes.routers import zabo_router
 from api.notifications.routers import noti_router
+from api.mails.routers import mail_router
 from api.users import views
 
 swagger_view = get_swagger_view(title="Pastebian API")
@@ -12,6 +13,7 @@ urlpatterns = (
     url(r'^api/', include(zabo_router.urls)),
     url(r'^api/', include(zabouser_router.urls)),
     url(r'^api/', include(noti_router.urls)),
+    url(r'^api/', include(mail_router.urls)),
 
     url(r'^api/login/$', views.login),
     url(r'^api/login/callback/$', views.login_callback),
--- zabo/settings/components/mail.py
+++ zabo/settings/components/mail.py
@@ -1,2 +1,2 @@
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-ADMIN_MAIL = ""
+ADMIN_MAIL = "vk8520@kaist.ac.kr"
