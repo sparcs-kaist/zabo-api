03626e26eff7132c3381085e6c240a153c8161ac
jara <jara0701@gmail.com>
2019-01-23 17:31:57 +0900

    도커 파일시스템 인코딩 변경

--- docker_run.sh
+++ docker_run.sh
@@ -1,1 +1,1 @@
-docker run -it -v /home/jara/zabo/zabo_api/zabo_api:/home/zabo/zabo_api -p 16135:8000 -e LANG="ko_KR.UTF_8" vk8520/zabo-base:0.6
+docker run -it -v /home/jara/zabo/zabo_api/zabo_api:/home/zabo/zabo_api -p 16135:8000 -e LC_ALL=C.UTF-8 vk8520/zabo-base:0.6
